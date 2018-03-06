0318f7dc4e6148d3c0700650add326af74235dc1
Jsinclairisto <jakflet@aol.com>
2018-03-06 12:21:51 -0800

    second commit

--- Web_Scrape/web_scrape.rb
+++ Web_Scrape/web_scrape.rb
@@ -5,23 +5,17 @@ url = "http://boards.4chan.org/f/"
 
 document = open(url)
 content = document.read
-parsed_content = Nokogiri::HTML(content)
+doc = Nokogiri::HTML(content)
 
-parsed_content.css('.board').css('.highlightPost').each do |highlightPost|
+doc.css('.board').css('.highlightPost').each do |highlightPost|
 	title		 = highlightPost.css('.file-col').inner_text
 	user_name 	 = highlightPost.css('.name-col').inner_text
-	width 		 = highlightPost.css('.data-width').inner_text
-	height 		 = highlightPost.css('.data-height').inner_text
 	subject		 = highlightPost.css('.subject').inner_text
-	
-	
+    
 	puts title
 	puts user_name
-	puts width
-	puts height
-	puts subject
-	puts '------------------------------------------'
-
+    puts subject
+    puts '------------------------------------------'
 
 
 end
